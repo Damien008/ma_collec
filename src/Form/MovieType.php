1e43043d8dad283e4b26fb85a8427c783004fd1f
Damien008 <damien.lambert999@gmail.com>
2021-02-03 13:54:12 +0100

    feat make:crud

---
+++ src/Form/MovieType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Movie;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MovieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('poster')
+            ->add('genre')
+            ->add('synopsis')
+            ->add('country')
+            ->add('support')
+            ->add('duration')
+            ->add('trailer')
+            ->add('owner')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Movie::class,
+        ]);
+    }
+}
