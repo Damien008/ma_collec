31c73fe061ba40158263595591c771c94167895c
Damien008 <damien.lambert999@gmail.com>
2021-02-03 13:40:28 +0100

    new migration

---
+++ migrations/Version20210203123611.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210203123611 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE movie (id INT AUTO_INCREMENT NOT NULL, owner_id INT DEFAULT NULL, title VARCHAR(55) NOT NULL, poster VARCHAR(255) NOT NULL, genre VARCHAR(55) NOT NULL, synopsis LONGTEXT DEFAULT NULL, country VARCHAR(55) NOT NULL, support VARCHAR(55) NOT NULL, duration DATE NOT NULL, trailer VARCHAR(255) DEFAULT NULL, INDEX IDX_1D5EF26F7E3C61F9 (owner_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE movie ADD CONSTRAINT FK_1D5EF26F7E3C61F9 FOREIGN KEY (owner_id) REFERENCES user (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE movie');
+    }
+}
